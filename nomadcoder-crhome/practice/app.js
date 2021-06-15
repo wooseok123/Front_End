938816145da60cb51103c98dbf0add67a03f8a1a
wooseok123 <59460718+wooseok123@users.noreply.github.com>
2021-06-15 23:53:31 +0900

    Add files via upload
    nomad coder 를 들으며 얻는 꿀팁들

---
+++ nomadcoder-crhome/practice/app.js
@@ -0,0 +1,59 @@
+// prompt 는 css를 적용할 수 없고 제한적이므로 잘 사용하지 않는다.
+    //    게다가 입력값을 적기 전까진 무한 로딩이다.
+
+// typeof variable -> 해당 변수의 데이터 자료형을 볼 수 있다.
+    // 띄어쓰기 해야함!
+
+// str->int -> parseInt() + str -> int = NaN!(Not a Number)
+
+// var은 절대 쓰지 말고, 대게 const를 쓰며 업데이트 할 필요가 있으면
+ // let을 쓰도록
+
+ // isNaN -> 함수인데, 인자를 하나 주면 숫자인지 아닌지 알려줌
+   // 당연히 return 값은 boolean
+
+/*
+-----------------------
+
+else if(조건문) {
+ 조건이 맞을 때 실행될 문
+};
+
+== elif
+
+-----------------------
+
+and = && 
+or = ||
+
+-------------------------
+
+= -> allocate value
+== -> are they same? (not type)
+=== -> are they 'literally' same? (value+type)
+
+-------------------------------------------------
+
+document -> 연결된 hmtl의 문서 전체를 가져옴
+즉, html element를 모두 변경, 수정, 읽을 수 있다
+
+dir() -> ojbect화 시킴 
+
+ex) console.dir(document)
+
+------------------------------------------------------
+
+html에서 id를 어떻게 가져올까?
+document.getElementById("id");
+ -> 해당 id를 가진 구역을 모두 가져옴 
+ 
+ 안의 내용을 바꾸고 싶다
+ -> title.innerText = "바꿀 내용"
+ 
+ -------------------------------------------------
+
+ 
+ */
+
+
+ 
