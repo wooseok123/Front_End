4fa93a728ab97c0de18c34e2ebcab6a933a210f5
wooseok123 <59460718+wooseok123@users.noreply.github.com>
2021-06-19 20:06:57 +0900

    Add files via upload

---
+++ nomadcoder-crhome/practice/practice.js
@@ -0,0 +1,8 @@
+const loginInput = document.querySelector(".login-form input")
+const loginBtn = document.querySelector(".login-form button")
+
+function handleBtnClick() {
+    
+}
+
+loginBtn.addEventListener("click",handleBtnClick);
