8767862221d9186835c1b035431965471dcf15a3
wooseok123 <59460718+wooseok123@users.noreply.github.com>
2021-10-11 14:44:42 +0900

    just for navBar

---
+++ ongdalssam/main.js
@@ -0,0 +1,15 @@
+// make navBar background highlight when scrolling over
+
+const navList = []
+const navBarList = document.querySelectorAll(".navBar__menu__item")
+  .forEach((nav) => navList.push(nav));
+
+// function changeBgColor(event) {
+//   event.target.classList.toggle("transition");
+//   // event.target.style.backgroundColor = "#FEF9EF";
+// } 
+
+// navList.forEach((nav) => nav.addEventListener('mouseover',changeBgColor));
+
+// // navList.addEventListener("mouse")
+
