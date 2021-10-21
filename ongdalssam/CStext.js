8ff9b16c635b51833232a8f422f1d1562729f662
wooseok123 <59460718+wooseok123@users.noreply.github.com>
2021-10-21 23:58:50 +0900

    js : multiple modal!
    done~

---
+++ ongdalssam/CStext.js
@@ -0,0 +1,3 @@
+// 
+
+export const plus = (a,b) => a+b;
--- ongdalssam/main.js
+++ ongdalssam/main.js
@@ -1,3 +1,7 @@
+
+
+const CStexts = ["생수를 마셔 미네랄을 보충하기 위해서는 상당히 많은 양의 물을 먹어야 합니다. 따라서 미네랄의 총 함량보다는, 각각의 무기물 함량을 봐야 합니다."];
+
 // make navBar background highlight when scrolling over
 
 const navList = []
@@ -13,3 +17,23 @@ const navBarList = document.querySelectorAll(".navBar__menu__item")
 
 // // navList.addEventListener("mouse")
 
+
+const waterCS_contents = Array.from(document.querySelectorAll(".waterCS__content"));
+const modal_title = document.querySelector(".modal_title");
+const modal_content = document.querySelector(".modal_content");
+
+waterCS_contents.forEach( (item) => item.addEventListener("click",(event) => {
+  console.log(event);
+  const title = event.target.outerText;
+  const index = waterCS_contents.indexOf(event.path[1]);
+  modal_title.innerHTML = title;
+  modal_content.textContent = CStexts[index];
+  modalContainer.style.display = "block";
+}));
+
+const modalContainer = document.querySelector(".CS_modal");
+
+const modalClose = document.querySelector(".close");
+modalClose.addEventListener("click",() => {
+  modalContainer.style.display = "none"
+})
