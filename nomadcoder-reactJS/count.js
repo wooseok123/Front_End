3ea2dff0443ca69883292a7753b54dce96ec16ae
Wooseok <hosmimam@naver.com>
2022-02-13 16:18:39 +0900

    make components

--- nomadcoder-reactJS/count.js
+++ nomadcoder-reactJS/count.js
@@ -1,9 +1,52 @@
-const countBtn = document.getElementById("countBtn");
-const countNum = document.getElementById("countNum");
+const root = document.getElementById("root");
+const Title = () => {
+  return (
+    <h3 id="title" onMouseEnter={() => console.log("mouse enter")}>
+      Hello I'm a title
+    </h3>
+  );
+};
+/*
+        const h3 = React.createElement(
+            "h3",
+            {
+                onMouseEnter: () => console.log("mouse enter"),
+            },
+            "Hello i'm span"
+        );
+        */
+const Button = () => {
+  return (
+    <button
+      style={{
+        backgroundColor: "tomato",
+      }}
+      onClick={() => console.log("i'm clicked")}
+    >
+      Click me
+    </button>
+  );
+};
 
-let realNum = 0;
+/*       
+      const btn = React.createElement(
+        "button",
+        {
+          onClick: () => console.log("i'm clicked"),
+          style: {
+            backgroundColor: "tomato",
+          },
+        },
+        "Click me"
+      ); */
 
-countBtn.addEventListener("click",() => {
-    realNum++;
-    countNum.innerText = `Total Clicks : ${realNum}`;
-})
+const Container = (
+  <div>
+    <Title />
+    <Button />
+  </div>
+);
+ReactDOM.render(Container, root);
+
+/* const container = React.createElement("div", null, [Title, Button]);
+      ReactDOM.render(container, root); */
