703370505ae121cc9084a53c5de053c05e941952
Wooseok <hosmimam@naver.com>
2022-02-13 16:43:48 +0900

    the way that isn't good to re-rendering

--- nomadcoder-reactJS/count.js
+++ nomadcoder-reactJS/count.js
@@ -1,52 +1,9 @@
-const root = document.getElementById("root");
-const Title = () => {
-  return (
-    <h3 id="title" onMouseEnter={() => console.log("mouse enter")}>
-      Hello I'm a title
-    </h3>
-  );
-};
-/*
-        const h3 = React.createElement(
-            "h3",
-            {
-                onMouseEnter: () => console.log("mouse enter"),
-            },
-            "Hello i'm span"
-        );
-        */
-const Button = () => {
-  return (
-    <button
-      style={{
-        backgroundColor: "tomato",
-      }}
-      onClick={() => console.log("i'm clicked")}
-    >
-      Click me
-    </button>
-  );
-};
+const countBtn = document.getElementById("countBtn");
+const countNum = document.getElementById("countNum");
 
-/*       
-      const btn = React.createElement(
-        "button",
-        {
-          onClick: () => console.log("i'm clicked"),
-          style: {
-            backgroundColor: "tomato",
-          },
-        },
-        "Click me"
-      ); */
+let realNum = 0;
 
-const Container = (
-  <div>
-    <Title />
-    <Button />
-  </div>
-);
-ReactDOM.render(Container, root);
-
-/* const container = React.createElement("div", null, [Title, Button]);
-      ReactDOM.render(container, root); */
+countBtn.addEventListener("click", () => {
+  realNum++;
+  countNum.innerText = `Total Clicks : ${realNum}`;
+});
