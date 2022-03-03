a625eb88c661ca5182b297309bdbd838b3ea18bf
hanwooseok <hosmimam@naver.com>
2022-03-03 23:22:21 +0900

    make sound visualize

--- p5.js/animation.js
+++ p5.js/animation.js
@@ -1,17 +1,43 @@
-function  setup() {
-    createCanvas(1600, 800);
-    background(220);
+let mySound, fft, Width;
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  mySound = loadSound("material/credit.mp3");
 }
 
-let radius = 0;
+function setup() {
+    fft = new p5.FFT();
+    peakDetect = new p5.PeakDetect();
+    frameRate(30);
+    let cnv = createCanvas(windowWidth, windowHeight);
+    cnv.mousePressed(canvasPressed);
+    stroke('black');
+    text('tap here to play', windowWidth/2, windowHeight/2);
+}
 
 function draw() {
-    const randomX = random(400);
-    const randomY = random(800);
-    radius += 1;
-    circle(radius, cos(radius)*1600, 30);
-    // 뫼비우스의 띠가 돌아가게 만들고 싶엉
+
+
+    fft.analyze();
+    peakDetect.update(fft);
+
+    if ( peakDetect.isDetected ) {
+        Width = 100;
+    } else {
+        Width *= 0.8;
+    }
+    stroke(random(255),random(255),random(50)*5);
+    if (Width > 50) {
+        ellipse(random(windowWidth), random(windowHeight), Width);
+    } else {
+        rect(random(windowWidth), random(windowHeight), Width);
+    }
+
 }
 
-ㅎ
-
+function canvasPressed() {
+    if (mySound.isPlaying()) {
+        mySound.stop();
+    } else
+        mySound.play();
+}
