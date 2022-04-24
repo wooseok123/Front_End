22cc73e54ed9ce09f5d07fba9e53431de2cb7bb1
hanwooseok <hosmimam@naver.com>
2022-04-24 14:19:19 +0900

    Downgrad React Version to 17 and Upgrade react-router-dom to 6

--- website/Build_Websites/react_firebase/firebasezoom/src/components/Router.js
+++ website/Build_Websites/react_firebase/firebasezoom/src/components/Router.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Navigation from "./Navigation";
@@ -14,25 +9,18 @@ const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       {isLoggedIn && <Navigation />}
-      <Switch>
+      <Routes>
         {isLoggedIn ? (
           <>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
           </>
         ) : (
           <>
-            <Route path="/">
-              <Auth />
-            </Route>
-            <Redirect from="*" to="/" />
+            <Route path="/" element={<Auth />} />
           </>
         )}
-      </Switch>
+      </Routes>
     </Router>
   );
 };
