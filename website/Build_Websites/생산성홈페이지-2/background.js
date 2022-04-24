66ae3c6e9c9485dd3515bfea10c66e8bec73b861
hanwooseok <hosmimam@naver.com>
2022-04-24 10:56:55 +0900

    modifed

--- website/Build_Websites/생산성홈페이지-2/background.js
+++ website/Build_Websites/생산성홈페이지-2/background.js
@@ -2,26 +2,26 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 5;
 
-function handleImageLoad () {
-    console.log("finished loading");
+function handleImageLoad() {
+  console.log("finished loading");
 }
 
 function paintImage(imgNumber) {
-    const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add('bgImage');
-    body.prepend(image);
+  const image = new Image();
+  //image.src = `images/${imgNumber + 1}.jpg`;
+  image.src = `image/1.jpg`;
+  image.classList.add("bgImage");
+  body.prepend(image);
 }
 
 function getRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
 }
 
 function init() {
-    const randomNumber = getRandom();
-    paintImage(randomNumber);
-
+  const randomNumber = getRandom();
+  paintImage(randomNumber);
 }
 
 init();
